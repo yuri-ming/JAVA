3bc5d1bc1a93359737b84ddda062338d05715808
LAPTOP-UBOAB7BN\SAMSUNG <lul6959@naver.com>
2021-04-05 15:43:06 +0900

    210405 array

---
+++ Tutorial8/src/Main2.java
@@ -0,0 +1,18 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		int[] array= new int[100];
+		
+		for (int i =0;i<100;i++) {
+			array[i]=(int)(Math.random()*100+1);
+		}
+		int sum=0;
+		for(int i=0;i<100;i++) {
+			sum += array[i];
+		}
+		System.out.println("100개의 랜덤 정수의 평균 값은 "+sum/100);
+	}
+	
+
+}
